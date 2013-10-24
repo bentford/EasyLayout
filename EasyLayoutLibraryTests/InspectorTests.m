5af43c5be767005a2cce3fb3a266b950dbe39912
Ben Ford <bentford@gmail.com>
2013-10-24 16:39:34 -0700

    Added new test suite for inspector

---
+++ EasyLayoutLibraryTests/InspectorTests.m
@@ -0,0 +1,34 @@
+//
+//  InspectorTests.m
+//  EasyLayoutLibrary
+//
+//  Created by Ben Ford on 10/24/13.
+//  Copyright (c) 2013 Ben Ford. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InspectorTests : XCTestCase
+
+@end
+
+@implementation InspectorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
